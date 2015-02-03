34c68c80177bad562023d134a20a1ff43dd46de0
Charkee Berekeke <admin@brekenet.com>
2015-02-03 02:00:09 -0500

    draft for scp

--- myssh.py
+++ myssh.py
@@ -7,15 +7,22 @@ import socket
 
 islist = lambda x: (type(x) is list) and x or []
 
+
+def load_keys(path="~/.ssh/known_hosts"):
+    d = os.path.expanduser(path)
+
+    if not os.path.exists(os.path.dirname(d)):
+        os.makedirs(os.path.dirname(d))
+
+    if not os.path.exists(d):
+        open(d, 'a').close()
+        
+    paramiko.util.load_host_keys(d)
+
 def ssh(ip, port=22, creds=None, pre_cmds=None, cmds=None):
     """
     Function for ssh'ing into a device using paramiko
     """
-    d = os.path.expanduser('~/.ssh/known_hosts')
-    if not os.path.exists(d):
-        os.makedirs(os.path.dirname(d))
-        open(d, 'a').close()
-    keys = paramiko.util.load_host_keys(d)
 
     i = 0
     pre_cmds = islist(pre_cmds)
@@ -27,7 +34,7 @@ def ssh(ip, port=22, creds=None, pre_cmds=None, cmds=None):
             t = paramiko.Transport((ip, port))
             t.start_client()
             auth = t.auth_password(creds[i][0], creds[i][1])
-        except paramiko.AuthenticationException:
+        except paramiko.ssh_exception.AuthenticationException:
             i+=1
         except (socket.error, paramiko.ssh_exception.SSHException) as e:
             return False, e
@@ -67,3 +74,16 @@ def send_cmd(chan, cmds=None, delay=1, buf_size=1000):
             buf += chan.recv(buf_size)
 
     return chan, buf 
+
+def scp(ip, port=22, creds=None, src=None, dst=None):
+    i = 0
+    while True:
+        try:
+            t = paramiko.Transport((ip, port))
+            t.connect(username=creds[i][0], password=creds[i][1])
+        except paramiko.ssh_exception.AuthenticationException:
+            i+=1
+        except (socket.error, paramiko.ssh_exception.SSHException) as e:
+            return False, e
+        else:
+            break
