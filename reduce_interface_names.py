dfa222c30c4fae55c35178cb0d084d1981c29285
Charkee Berekeke <admin@brekenet.com>
2015-05-01 12:22:26 +1000

    reduce_interface_names.py now in usable form

--- reduce_interface_names.py
+++ reduce_interface_names.py
@@ -3,47 +3,47 @@
 import os
 import re
 from string import maketrans, translate
+from operator import itemgetter
+from itertools import groupby
 
-IFACE_CHARS = r'^([a-zA-Z-]*)'
-IFACE_DELIMS = '/:'
+IFACE_DELIMS = '(^[a-zA-Z-]+|/|:)'
 
 def breakdown(iface):
-    _iface = re.sub(IFACE_CHARS, r'\1 ', iface)
-    _iface = translate(_iface, maketrans(IFACE_DELIMS, " "*len(IFACE_DELIMS)))
+    _iface = re.sub(IFACE_DELIMS, r' \1 ', iface).strip()
     return _iface.split()
 
-def insert_in_range(base, new):
-    _new = int(new)
-    if "-" in base:
-        a, z = map(int, base.split("-"))
-        if _new > z:
-            if _new - z > 1:
-                ret = base + "," + new
-            else:
-                ret = str(a) + "-" + new
-        elif _new < a:
-            if a - _new > 1:
-                ret = new + "," + base
-            else:
-                ret = new + "-" + str(z)
-        else:
-            return base
+def to_range(s):
+    if "-" in s:
+        x, y = map(int, s.split("-"))
+        return range(x, y+1)
     else:
-        _base = int(base)
-        if _new > _base:
-            z, a = _new, _base
-        else:
-            z, a = _base, _new
-        if z - a > 1:
-            ret = str(a) + "," + str(z)
-        else:
-            ret = str(a) + "-" + str(z)
-    return ret
+        return [int(s)]
 
-def reduce(base, new):
+def from_range(li):
+    s = []
+    for l in li:
+        if len(l) == 1:
+            s.append(str(l[0]))
+        else:
+            s.append("%s-%s" % (str(l[0]), str(l[-1])))
+    return ",".join(s)
+
+def insert_in_range(base, new):
+    tmp = []
+    ranges = []
+    for i in base.split(","):
+        tmp.extend(to_range(i))
+    tmp.append(int(new))
+    tmp.sort()
+    tmp = list(set(tmp)) # remove duplicates
+    ranges = []
+    for k, g in groupby(enumerate(tmp), lambda (i, x): i-x):
+        ranges.append(map(itemgetter(1), g))
+    return from_range(ranges)
+
+def reduce_iface(base, new):
     _new = breakdown(new)
-    _base = base.split(",")
-    #insert = True
+    _base = (base != "") and base.split(";") or []
     for i in range(len(_base)):
         _b = breakdown(_base[i])
         if len(_b) != len(_new):
@@ -52,8 +52,8 @@ def reduce(base, new):
             continue
         else:
             _b[-1] = insert_in_range(_b[-1], _new[-1])
-            _base[i] = "".join(_b) # need to insert original delimiters
-            return ",".join(_base)
+            _base[i] = "".join(_b)
+            return ";".join(_base)
     _base.append(new)
-    return ",".join(_base)
+    return ";".join(_base)
 
