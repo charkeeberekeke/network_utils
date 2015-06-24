5e41acfcc0a0ac802bfeb41edf47e8cc7d46d60d
Charkee Berekeke <admin@brekenet.com>
2015-06-24 21:52:47 +1000

    Changes to collect_output and timeout

--- cisco_ssh.py
+++ cisco_ssh.py
@@ -10,13 +10,14 @@ class CiscoSSH:
     BUFFER = 5000
     RETRIES = 5 
 
-    def __init__(self, creds=None, ip=None, port=None, path_to_keys=None, logger=None):
+    def __init__(self, creds=None, ip=None, port=None, path_to_keys=None, logger=None, timeout=8):
         # need to add facility for username. password combos
         self.ip = ip
         self.port = port or 22
         self.creds = creds
         self.chan = None
         self.logger = logger or logging.getLogger(__name__)
+        self.timeout = timeout
 
         self.load_keys(path=path_to_keys)
 #        self.authenticate()
@@ -40,7 +41,7 @@ class CiscoSSH:
         i = 0
         while True:
             try:
-                print self.ip, self.port
+                #print self.ip, self.port
                 t = paramiko.Transport((self.ip, self.port))
                 t.start_client()
                 auth = t.auth_password(self.creds[i][0], self.creds[i][1])
@@ -55,22 +56,22 @@ class CiscoSSH:
         self.chan.get_pty()
         self.chan.invoke_shell()
         self.chan.setblocking(0)
-        self.chan.settimeout(8)
+        self.chan.settimeout(self.timeout)
 
         if pre_cmds:
             self.send_cmd(pre_cmds)
 
         return True, self.creds[i][0]
 
-    def collect_output(self):
+    def collect_output(self, delay_factor=1):
         _buf = ""
         for i in range(CiscoSSH.RETRIES):
-            time.sleep(CiscoSSH.DELAY)
-            while self.chan.recv_ready():
-                    _buf += self.chan.recv(CiscoSSH.BUFFER)
+            time.sleep(CiscoSSH.DELAY * delay_factor)
+            while self.chan and self.chan.recv_ready():
+                _buf += self.chan.recv(CiscoSSH.BUFFER)
         return _buf
 
-    def send_cmd(self, cmds=None):
+    def send_cmd(self, cmds=None, delay_factor=1):
         cmds = self.__islist(cmds)
         buf = ""
 
@@ -78,6 +79,9 @@ class CiscoSSH:
 
         for cmd in cmds:
             self.chan.send(cmd + "\n")
-            buf += self.collect_output()
+            buf += self.collect_output(delay_factor)
 
         return buf
+
+    def close(self):
+        self.chan.close()
